a3e0d62ed72522709f1206955b4ee217bcc27df3
Bochkarev Ivan <ivanx86@gmail.com>
2020-10-28 16:43:35 +0600

    Update NextJS, new pages Strapi, PWA

--- front/postcss.config.js
+++ front/postcss.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer', 'postcss-preset-env'],
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
 }
